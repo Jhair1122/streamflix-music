b07044f4c752861655487510c32f693a1e84637d
Jhair1122 <jhairurbina15@gmail.com>
2026-06-04 09:31:00 -0500

    Create db.py

---
+++ db.py
@@ -0,0 +1,7 @@
+import os
+from supabase import create_client, Client
+
+SUPABASE_URL = os.getenv("SUPABASE_URL", "https://jhlktvdylbiieeuwykgj.supabase.co")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpobGt0dmR5bGJpaWVldXd5a2dqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAzMzIwNjMsImV4cCI6MjA5NTkwODA2M30.jie5MZF36VXhsfEZggCCWJ3M5HQVShGmyss6f-nLa3s")
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
